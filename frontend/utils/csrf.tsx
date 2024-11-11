21397029aa5bc8f43702492cd07b810bbd0fb800
DavidSlva <68710769+DavidSlva@users.noreply.github.com>
2024-11-11 07:59:36 -0300

    bandas udp proyecto

---
+++ frontend/utils/csrf.tsx
@@ -0,0 +1,7 @@
+export function getCSRFToken() {
+    const csrfCookie = document.cookie
+        .split('; ')
+        .find((row) => row.startsWith('csrftoken='));
+
+    return csrfCookie ? csrfCookie.split('=')[1] : null;
+}
